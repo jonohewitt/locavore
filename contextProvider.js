5fdd4344ffbf1bd4e71e2ffb20eee08c5b4a97e5
Jono Hewitt <jonohewitt@gmail.com>
2020-08-07 00:01:55 +0100

    Fix styling issues on load

--- contextProvider.js
+++ contextProvider.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import localStorageMemory from "localstorage-memory"
 
 export const myContext = React.createContext()
 
-const windowGlobal = typeof window !== "undefined" && window
-const storage = windowGlobal ? windowGlobal.localStorage : localStorageMemory
-
-const localTheme = storage.getItem("darkTheme") === "true"
-
-const systemTheme =
-  windowGlobal.matchMedia &&
-  windowGlobal.matchMedia("(prefers-color-scheme: dark)").matches &&
-  storage.getItem("darkTheme") === null
-
 const Provider = props => {
+  const windowGlobal = typeof window !== "undefined" && window
+  const storage = windowGlobal ? windowGlobal.localStorage : localStorageMemory
+
+  const localTheme = storage.getItem("darkTheme") === "true"
+
+  const systemTheme =
+    windowGlobal.matchMedia &&
+    windowGlobal.matchMedia("(prefers-color-scheme: dark)").matches &&
+    storage.getItem("darkTheme") === null
+
   const [isDark, setTheme] = useState(localTheme || systemTheme)
   const [settingsOpen, toggleSettings] = useState(false)
 

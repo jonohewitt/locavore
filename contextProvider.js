98c227ef19dd11f0d25964951132ade37b2e4084
Jono Hewitt <jonohewitt@gmail.com>
2020-08-06 21:59:08 +0100

    Persistent colour theme when chosen, defaults to system pref if unset

--- contextProvider.js
+++ contextProvider.js
@@ -1,25 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
-export const myContext = React.createContext();
+export const myContext = React.createContext()
+
+const localTheme = window.localStorage.getItem("darkTheme") === "true"
+console.log(localTheme)
+
+const systemTheme =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches &&
+  window.localStorage.getItem("darkTheme") === null
 
 const Provider = props => {
-  const [isDark, setTheme] = useState(false);
-  const [settingsOpen, toggleSettings] = useState(false);
+  const [isDark, setTheme] = useState(localTheme || systemTheme)
+  const [settingsOpen, toggleSettings] = useState(false)
 
   return (
-    <myContext.Provider value={{
-      isDark,
-      changeTheme: () => setTheme(!isDark),
-      settingsOpen,
-      toggleSettings: () => toggleSettings(!settingsOpen)
-    }}>
+    <myContext.Provider
+      value={{
+        isDark,
+        changeTheme: () => {
+          setTheme(!isDark)
+          window.localStorage.setItem("darkTheme", !isDark)
+        },
+        settingsOpen,
+        toggleSettings: () => toggleSettings(!settingsOpen),
+      }}
+    >
       {props.children}
     </myContext.Provider>
   )
-};
+}
 
-export default ({ element }) => (
-  <Provider>
-    {element}
-  </Provider>
-);
+export default ({ element }) => <Provider>{element}</Provider>

f7612b88640a97d81dd7ce9c9d0b6c984e10db24
Jono Hewitt <jonohewitt@gmail.com>
2020-08-10 16:31:28 +0100

    Add styled footer section, stick to end of page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,24 @@
 import React from "react"
 import styled from "styled-components"
+import ContentWrapper from "./contentWrapper"
+
+export const footerHeight = "150px"
 
 const FooterWrapper = styled.div`
-  margin-bottom: 30px;
+  background-color: var(--color-graphBackground);
+  height: ${footerHeight};
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 `
 
 const Footer = ({ siteTitle }) => (
   <FooterWrapper>
+    <ContentWrapper padding="50px 0 0 0">
     <p>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+      © {new Date().getFullYear()}, La Coloc' Locale
     </p>
+    </ContentWrapper>
   </FooterWrapper>
 )
 

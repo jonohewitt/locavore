7768eab7eb3a81942fc767f40615cc80784e87af
Jono Hewitt <jonohewitt@gmail.com>
2020-08-10 16:39:00 +0100

    Cleanup comments and move blog template path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const path = require("path")
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  // Destructure the createPage function from the actions object
+  
   const { createPage } = actions
 
   const result = await graphql(`
@@ -23,19 +23,15 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
   }
 
-  // Create blog post pages.
   const posts = result.data.allMdx.edges
 
-  // you'll call `createPage` for each result
   posts.forEach(({ node }, index) => {
     createPage({
-      // This is the slug you created before
-      // (or `node.frontmatter.slug`)
+
       path: node.frontmatter.slug,
-      // This component will wrap our MDX content
-      component: path.resolve(`./src/components/blog-template.js`),
-      // You can use the values in this context in
-      // our page layout component
+
+      component: path.resolve(`./src/blog/blog-template.js`),
+
       context: { id: node.id },
     })
   })

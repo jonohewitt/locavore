399fab1022e36809e9540acd9722352c9eec08af
Jono Hewitt <jonohewitt@gmail.com>
2020-08-13 12:20:15 +0100

    Add grid layout for desktop and tweak info shown on cards

--- src/components/recipeIndex.js
+++ src/components/recipeIndex.js
@@ -2,54 +2,66 @@ import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import Img from "gatsby-image"
+import {breakToMobile} from "./contentWrapper"
 
 const UL = styled.ul`
   margin-top: 50px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(327px, 1fr));
+  grid-gap: 25px;
+  @media (max-width: 905px) {
+    grid-row-gap: 35px;
+  }
+`
+
+const Li = styled.li`
+  height: 420px;
+  @media (max-width: 905px) {
+    height: 100%;
+  }
 `
 
 const Post = styled.div`
   background-color: var(--color-graphBackground);
-  margin: 20px 0;
   border-radius: 8px;
   box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
   transition: transform 0.3s;
   overflow: hidden;
+  @media (min-width: ${breakToMobile}px) {
+    height: 100%;
+  }
+
 
   &:hover {
     transform: translateY(-5px);
   }
 `
-const PostHeader = ({
-  headerImg,
-  headerDesc,
-  featureImg,
-  featureDesc,
-}) => {
-  const headerArray = [headerImg, headerDesc]
-  const featureArray = [featureImg, featureDesc]
+const PostHeader = ({ headerImg, headerDesc, featureImg, featureDesc }) => {
+  const header = { image: headerImg, description: headerDesc }
+  const feature = { image: featureImg, description: featureDesc }
 
-let postArray = false;
+  let postImage = false
 
-  if (featureArray[0] && !headerArray[0]){
-     postArray = featureArray;
-  } else if (headerArray[0]) {
-     postArray = headerArray
+  if (feature.image && !header.image) {
+    postImage = feature
+  } else if (header.image) {
+    postImage = header
   }
 
-  if (postArray) {
+  if (postImage) {
     return (
       <Img
         style={{
           width: "100%",
-          height: "150px",
+          maxHeight: "200px",
         }}
         imgStyle={{
           objectFit: "cover",
           width: "100%",
           height: "100%",
         }}
-        fluid={postArray[0]}
-        alt={postArray[1] ? postArray[1] : ""}
+        fluid={postImage.image}
+        alt={postImage.description ? postImage.description : ""}
       />
     )
   } else {
@@ -62,6 +74,7 @@ const PostText = styled.div`
 
   h2 {
     margin: 0;
+    padding-bottom: 15px;
   }
 `
 
@@ -72,11 +85,14 @@ const RecipeIndexWrapper = styled.div`
 const RecipeIndex = () => {
   const data = useStaticQuery(graphql`
     query RecipeIndexQuery {
-      allMdx(filter: {frontmatter: {category: {eq: "recettes"}}}, sort: {fields: frontmatter___date, order: DESC}) {
+      allMdx(
+        filter: { frontmatter: { category: { eq: "recettes" } } }
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
         edges {
           node {
             id
-            excerpt(pruneLength: 200)
+            excerpt(pruneLength: 100)
             frontmatter {
               title
               category
@@ -108,54 +124,53 @@ const RecipeIndex = () => {
   return (
     <RecipeIndexWrapper>
       <h2>Recettes</h2>
-      <hr/>
-    <UL>
-      {data.allMdx.edges.map(post => {
-        const headerIsIncluded = post.node.frontmatter.header !== null
-        const headerImg = headerIsIncluded
-          ? post.node.frontmatter.header.childImageSharp.fluid
-          : false
+      <hr />
+      <UL>
+        {data.allMdx.edges.map(post => {
+          const headerIsIncluded = post.node.frontmatter.header !== null
+          const headerImg = headerIsIncluded
+            ? post.node.frontmatter.header.childImageSharp.fluid
+            : false
 
-        const headerDescIsIncluded =
-          post.node.frontmatter.headerDescription !== null
-        const headerDesc = headerDescIsIncluded
-          ? post.node.frontmatter.headerDescription
-          : false
+          const headerDescIsIncluded =
+            post.node.frontmatter.headerDescription !== null
+          const headerDesc = headerDescIsIncluded
+            ? post.node.frontmatter.headerDescription
+            : false
 
-        const featureIsIncluded = post.node.frontmatter.feature !== null
-        const featureImg = featureIsIncluded
-          ? post.node.frontmatter.feature.childImageSharp.fluid
-          : false
+          const featureIsIncluded = post.node.frontmatter.feature !== null
+          const featureImg = featureIsIncluded
+            ? post.node.frontmatter.feature.childImageSharp.fluid
+            : false
 
-        const featureDescIsIncluded =
-          post.node.frontmatter.featureDescription !== null
-        const featureDesc = featureDescIsIncluded
-          ? post.node.frontmatter.featureDescription
-          : false
+          const featureDescIsIncluded =
+            post.node.frontmatter.featureDescription !== null
+          const featureDesc = featureDescIsIncluded
+            ? post.node.frontmatter.featureDescription
+            : false
 
-        return (
-          <li key={post.node.id}>
-            <Link
-              to={`/${post.node.frontmatter.category}${post.node.frontmatter.slug}`}
-            >
-              <Post>
-                <PostHeader
-                  headerImg={headerImg}
-                  headerDesc={headerDesc}
-                  featureImg={featureImg}
-                  featureDesc={featureDesc}
-                />
-                <PostText>
-                  <h2>{post.node.frontmatter.title}</h2>
-                  <p>{post.node.frontmatter.date}</p>
-                  <p>{post.node.excerpt}</p>
-                </PostText>
-              </Post>
-            </Link>
-          </li>
-        )
-      })}
-    </UL>
+          return (
+            <Li key={post.node.id}>
+              <Link
+                to={`/${post.node.frontmatter.category}${post.node.frontmatter.slug}`}
+              >
+                <Post>
+                  <PostHeader
+                    headerImg={headerImg}
+                    headerDesc={headerDesc}
+                    featureImg={featureImg}
+                    featureDesc={featureDesc}
+                  />
+                  <PostText>
+                    <h2>{post.node.frontmatter.title}</h2>
+                    <p>{post.node.excerpt}</p>
+                  </PostText>
+                </Post>
+              </Link>
+            </Li>
+          )
+        })}
+      </UL>
     </RecipeIndexWrapper>
   )
 }

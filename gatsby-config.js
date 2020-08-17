218e55b76fa4003da1f6dbbab22518986a15d6a8
Jono Hewitt <jonohewitt@gmail.com>
2020-08-17 21:24:10 +0100

    Add author

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `La Coloc' Locale`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `La Coloc' Locale`,
   },
   plugins: [
     {

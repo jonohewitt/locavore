ab8485e6a6f006a48cb1d8dc6bba726c5aabc8fb
Jono Hewitt <jonohewitt@gmail.com>
2020-08-20 21:00:17 +0100

    Pull out ListOfRecipes component for reuse on ingredient pages

---
+++ src/components/listOfRecipes.js
@@ -0,0 +1,171 @@
+import React from "react"
+import { Link } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
+import TimeIndicators from "./recipeTimeInfo"
+import { breakToMobile } from "./contentWrapper"
+
+const StyledUL = styled.ul`
+  margin-top: 40px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(327px, 1fr));
+  grid-gap: 25px;
+  @media (max-width: 905px) {
+    grid-row-gap: 35px;
+  }
+`
+
+const RecipeCardContainer = styled.li`
+  height: 400px;
+  @media (max-width: 905px) {
+    height: 100%;
+  }
+`
+
+const RecipeCard = styled.div`
+  position: relative;
+  background-color: var(--color-graphBackground);
+  border-radius: 8px;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s;
+  overflow: hidden;
+  @media (min-width: ${breakToMobile}px) {
+    height: 100%;
+  }
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`
+
+const DairyLabel = styled.div`
+  height: 25px;
+  border-radius: 5px;
+  border: solid 1px;
+  color: ${props =>
+    props.vegan ? "var(--color-vegan)" : "var(--color-vegetarian)"};
+  position: absolute;
+  left: 20px;
+  bottom: 20px;
+  padding: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const DairyIndicator = ({ vegan, vegetarian }) => {
+  if (vegan) {
+    return <DairyLabel vegan>Vegan</DairyLabel>
+  } else if (vegetarian && !vegan) {
+    return <DairyLabel>Vegetarian</DairyLabel>
+  } else {
+    return false
+  }
+}
+
+const RecipeImage = ({ headerImg, featureImg }) => {
+  let usedImage = false
+
+  if (!headerImg.image && featureImg.image) {
+    usedImage = featureImg
+  } else if (headerImg.image) {
+    usedImage = headerImg
+  }
+
+  if (usedImage) {
+    return (
+      <Img
+        style={{
+          width: "100%",
+          maxHeight: "200px",
+        }}
+        imgStyle={{
+          objectFit: "cover",
+          width: "100%",
+          height: "100%",
+        }}
+        fluid={usedImage.image}
+        alt={usedImage.description ? usedImage.description : ""}
+      />
+    )
+  } else {
+    return false
+  }
+}
+
+const RecipeText = styled.div`
+  padding: 10px 20px 20px 20px;
+  margin-bottom: 20px;
+
+  hr {
+    margin-bottom: 15px;
+  }
+
+  h2 {
+    margin: 0;
+    padding-bottom: 8px;
+    line-height: 1.1;
+  }
+`
+
+const ListOfRecipes = ({ recipeList, filterList }) => (
+  <StyledUL>
+    {recipeList.map(post => {
+      const fm = post.frontmatter
+
+      const headerImg =
+        fm.header !== null ? fm.header.childImageSharp.fluid : false
+
+      const featureImg =
+        fm.feature !== null ? fm.feature.childImageSharp.fluid : false
+
+      const recipeMatch = () => (
+        <RecipeCardContainer key={post.id}>
+          <Link to={`/${fm.category}${fm.slug}`}>
+            <RecipeCard>
+              <RecipeImage
+                headerImg={{
+                  image: headerImg,
+                  description: fm.headerDescription,
+                }}
+                featureImg={{
+                  image: featureImg,
+                  description: fm.featureDescription,
+                }}
+              />
+              <RecipeText>
+                <h2>{fm.title}</h2>
+                <hr />
+                <p>{`${fm.course} • ${fm.feeds} people`}</p>
+              </RecipeText>
+              <DairyIndicator vegan={fm.vegan} vegetarian={fm.vegetarian} />
+              <TimeIndicators prepTime={fm.prepTime} cookTime={fm.cookTime} />
+            </RecipeCard>
+          </Link>
+        </RecipeCardContainer>
+      )
+
+      if (filterList) {
+        let recipeShouldBeIncluded = true
+
+        Object.entries(filterList).forEach(([filter, filterIsApplied]) => {
+          const recipePassesFilters =
+            fm[filter] || fm[filter] === filterIsApplied
+          if (!recipePassesFilters) {
+            recipeShouldBeIncluded = false
+          }
+        })
+
+        if (recipeShouldBeIncluded) {
+          return recipeMatch()
+        } else {
+          return false
+        }
+      } else {
+        return recipeMatch()
+      }
+    })}
+  </StyledUL>
+)
+
+export default ListOfRecipes
--- src/components/recipeIndex.js
+++ src/components/recipeIndex.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
-import Img from "gatsby-image"
-import { breakToMobile } from "./contentWrapper"
-import TimeIndicators from "./recipeTimeInfo"
 import { ShowFilters, Filters } from "./recipeFilters"
+import ListOfRecipes from "./listOfRecipes"
 
 const RecipeIndexWrapper = styled.div`
   margin-top: 50px;
@@ -26,108 +24,6 @@ const Header = styled.header`
   align-items: baseline;
 `
 
-const ListOfRecipes = styled.ul`
-  margin-top: 40px;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(327px, 1fr));
-  grid-gap: 25px;
-  @media (max-width: 905px) {
-    grid-row-gap: 35px;
-  }
-`
-
-const RecipeCardContainer = styled.li`
-  height: 400px;
-  @media (max-width: 905px) {
-    height: 100%;
-  }
-`
-
-const RecipeCard = styled.div`
-  position: relative;
-  background-color: var(--color-graphBackground);
-  border-radius: 8px;
-  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
-  transition: transform 0.3s;
-  overflow: hidden;
-  @media (min-width: ${breakToMobile}px) {
-    height: 100%;
-  }
-
-  &:hover {
-    transform: translateY(-5px);
-  }
-`
-const RecipeImage = ({ headerImg, featureImg }) => {
-  let usedImage = false
-
-  if (!headerImg.image && featureImg.image) {
-    usedImage = featureImg
-  } else if (headerImg.image) {
-    usedImage = headerImg
-  }
-
-  if (usedImage) {
-    return (
-      <Img
-        style={{
-          width: "100%",
-          maxHeight: "200px",
-        }}
-        imgStyle={{
-          objectFit: "cover",
-          width: "100%",
-          height: "100%",
-        }}
-        fluid={usedImage.image}
-        alt={usedImage.description ? usedImage.description : ""}
-      />
-    )
-  } else {
-    return false
-  }
-}
-
-const RecipeText = styled.div`
-  padding: 10px 20px 20px 20px;
-  margin-bottom: 20px;
-
-  hr {
-    margin-bottom: 15px;
-  }
-
-  h2 {
-    margin: 0;
-    padding-bottom: 8px;
-    line-height: 1.1;
-  }
-`
-
-const DairyLabel = styled.div`
-  height: 25px;
-  border-radius: 5px;
-  border: solid 1px;
-  color: ${props =>
-    props.vegan ? "var(--color-vegan)" : "var(--color-vegetarian)"};
-  position: absolute;
-  left: 20px;
-  bottom: 20px;
-  padding: 4px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
-const DairyIndicator = ({ vegan, vegetarian }) => {
-  if (vegan) {
-    return <DairyLabel vegan>Vegan</DairyLabel>
-  } else if (vegetarian && !vegan) {
-    return <DairyLabel>Vegetarian</DairyLabel>
-  } else {
-    return false
-  }
-}
-
 const RecipeIndex = ({ filterList, setFilterList }) => {
   const data = useStaticQuery(graphql`
     query RecipeIndexQuery {
@@ -135,36 +31,34 @@ const RecipeIndex = ({ filterList, setFilterList }) => {
         filter: { frontmatter: { category: { eq: "recettes" } } }
         sort: { fields: frontmatter___date, order: DESC }
       ) {
-        edges {
-          node {
-            id
-            frontmatter {
-              title
-              category
-              slug
-              header {
-                childImageSharp {
-                  fluid(maxWidth: 800) {
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
+        nodes {
+          id
+          frontmatter {
+            title
+            category
+            slug
+            header {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid_withWebp
                 }
               }
-              headerDescription
-              feature {
-                childImageSharp {
-                  fluid(maxWidth: 800) {
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
-                }
-              }
-              featureDescription
-              vegan
-              vegetarian
-              prepTime
-              cookTime
-              feeds
-              course
             }
+            headerDescription
+            feature {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+            featureDescription
+            vegan
+            vegetarian
+            prepTime
+            cookTime
+            feeds
+            course
           }
         }
       }
@@ -198,55 +92,8 @@ const RecipeIndex = ({ filterList, setFilterList }) => {
         toggleFilter={toggleFilter}
       />
 
-      <ListOfRecipes>
-        {data.allMdx.edges.map(post => {
-          const fm = post.node.frontmatter
+      <ListOfRecipes recipeList={data.allMdx.nodes} filterList={filterList}/>
 
-          const headerImg =
-            fm.header !== null ? fm.header.childImageSharp.fluid : false
-
-          const featureImg =
-            fm.feature !== null ? fm.feature.childImageSharp.fluid : false
-
-          const recipeMatch = () => (
-            <RecipeCardContainer key={post.node.id}>
-              <Link to={`/${fm.category}${fm.slug}`}>
-                <RecipeCard>
-                  <RecipeImage
-                    headerImg={{
-                      image: headerImg,
-                      description: fm.headerDescription,
-                    }}
-                    featureImg={{
-                      image: featureImg,
-                      description: fm.featureDescription,
-                    }}
-                  />
-                  <RecipeText>
-                    <h2>{fm.title}</h2>
-                    <hr />
-                    <p>{`${fm.course} • ${fm.feeds} people`}</p>
-                  </RecipeText>
-                  <DairyIndicator vegan={fm.vegan} vegetarian={fm.vegetarian} />
-                  <TimeIndicators
-                    prepTime={fm.prepTime}
-                    cookTime={fm.cookTime}
-                  />
-                </RecipeCard>
-              </Link>
-            </RecipeCardContainer>
-          )
-
-          if (
-            (fm.vegan === true || fm.vegan === filterList.vegan) &&
-            (fm.vegetarian === true || fm.vegan === filterList.vegetarian)
-          ) {
-            return recipeMatch()
-          } else {
-            return false
-          }
-        })}
-      </ListOfRecipes>
     </RecipeIndexWrapper>
   )
 }

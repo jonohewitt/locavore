27a6d1634ce812ba176c1e439084f1604c8971cf
Jono Hewitt <jonohewitt@gmail.com>
2020-08-21 10:39:57 +0100

    Add spacing wrapper

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
 import React from "react"
 
-// import Layout from "../components/layout"
+import ContentWrapper from "../components/contentWrapper"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <ContentWrapper>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn't exist... the sadness.</p>
+    </ContentWrapper>
   </>
 )
 

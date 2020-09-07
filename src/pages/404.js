94bd6f2908b355d893d94336799cc1d568f4e6f4
Jono Hewitt <jonohewitt@gmail.com>
2020-09-07 15:30:22 +0100

    Named imports, cleanup

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,14 @@
-import React, { useContext } from "react"
-import ContentWrapper from "../components/contentWrapper"
-import SEO from "../components/seo"
-import styled from "styled-components"
-import { GlobalState } from "../context/globalStateContext"
-
-const Container = styled.div`
-  margin-top: ${props => (props.appInterface ? "60px" : "120px")};
-`
+import React from "react"
+import { ContentWrapper } from "../components/contentWrapper"
+import { SEO } from "../components/seo"
 
 const NotFoundPage = () => {
-    const context = useContext(GlobalState)
   return (
     <>
       <SEO title="404: Not found" />
       <ContentWrapper>
-        <Container appInterface={context.appInterface}>
-          <h1>NOT FOUND</h1>
-          <p>You just hit a route that doesn't exist... the sadness.</p>
-        </Container>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn't exist... the sadness.</p>
       </ContentWrapper>
     </>
   )

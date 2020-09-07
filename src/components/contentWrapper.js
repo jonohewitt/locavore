7c0d55bde6d502e39b0fe202c41b04e5fc25e46e
Jono Hewitt <jonohewitt@gmail.com>
2020-09-07 15:31:55 +0100

    Centralise appInterface margin changes, rename vague variables

--- src/components/contentWrapper.js
+++ src/components/contentWrapper.js
@@ -1,23 +1,27 @@
-import React from "react"
+import React, { useContext } from "react"
 import styled from "styled-components"
+import { GlobalState } from "../context/globalStateContext"
 
-export const width = 75
+export const widthPercent = 75
+export const mobileWidthPercent = 90
 export const maxWidth = 750
 export const breakToMobile = 900
 
 const Wrapper = styled.div`
-  width: ${width}%;
+  width: ${widthPercent}%;
   max-width: ${maxWidth}px;
-  margin: 0 auto;
+  margin: ${props => (props.appInterface || props.headerImg ? "50px" : "100px")}
+    auto 0 auto;
   @media (max-width: ${breakToMobile}px) {
-    width: 90%;
+    width: ${mobileWidthPercent}%;
   }
 `
 
-const ContentWrapper = ({ children }) => {
+export const ContentWrapper = ({ children, headerImg }) => {
+  const context = useContext(GlobalState)
   return (
-    <Wrapper>{children}</Wrapper>
+    <Wrapper headerImg={headerImg} appInterface={context.appInterface}>
+      {children}
+    </Wrapper>
   )
 }
-
-export default ContentWrapper

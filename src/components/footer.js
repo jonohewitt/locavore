094b957431e24c2a5929ee2e2884806fd3eac1b0
Jono Hewitt <jonohewitt@gmail.com>
2020-09-07 15:31:21 +0100

    Refactor code to reflect changes to contentWrapper

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,28 +1,31 @@
 import React from "react"
 import styled from "styled-components"
-import ContentWrapper from "./contentWrapper"
+import { widthPercent, mobileWidthPercent, breakToMobile, maxWidth } from "./contentWrapper"
 
 export const footerHeight = 100
 const footerHeightString = `${footerHeight}px`
 
 const FooterWrapper = styled.footer`
-  background-color: var(--color-graphBackground);
+  background-color: var(--color-footer);
   height: ${footerHeightString};
   position: absolute;
   bottom: 0;
   width: 100%;
   display: flex;
   align-items: center;
+
+  p {
+    width: ${widthPercent}%;
+    max-width: ${maxWidth}px;
+    margin: 0 auto;
+    @media (max-width: ${breakToMobile}px) {
+      width: ${mobileWidthPercent}%;
+    }
+  }
 `
 
-const Footer = ({ siteTitle }) => (
+export const Footer = ({ siteTitle }) => (
   <FooterWrapper>
-    <ContentWrapper>
-    <p>
-      © {new Date().getFullYear()}, La Coloc' Locale
-    </p>
-    </ContentWrapper>
+    <p>© {new Date().getFullYear()}, La Coloc' Locale</p>
   </FooterWrapper>
 )
-
-export default Footer

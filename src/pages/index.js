8fb79d81b5d867d379181dfd0034620eb48aa58f
Jono Hewitt <jonohewitt@gmail.com>
2020-09-12 18:59:46 +0100

    Replace placeholder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react"
 import styled from "styled-components"
 import { vegetableData } from "../components/seasonalChartData"
-import { SeasonalVegChart } from "../components/seasonalChart"
 import { SEO } from "../components/seo"
 import { ContentWrapper } from "../components/contentWrapper"
 import { GlobalState } from "../context/globalStateContext"
@@ -29,31 +28,25 @@ const IndexPage = () => {
           <hr />
         </header>
         <article>
-          <SeasonalVegChart data={vegetableData} />
-
-          <p>Bienvenue à la coloc' locale :)</p>
-
+          <br />
           <p>
-            Comme son nom l'indique, à l'origine, nous sommes une colocation ;
-            trois amies, travailleuses et étudiante, chacune avec sa vie
-            personnelle, un emploi du temps plus ou moins chargé et vivant
-            ensemble au centre-ville à Bruxelles.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
 
           <p>
-            L'idée de la création d'une page nous est venue après une longue
-            période de réflexion car on se pose beaucoup de questions (peut-être
-            un peu trop parfois^^) sur nos modes de vie, la manière dont nous
-            consommons et nos petites (et grosses) incohérences d'êtres humains.
-            Voyant que nos réflexions et interrogations intéressaient aussi
-            d'autres personnes de notre entourage, nous avons décidé de partager
-            nos expériences de ces dernières semaines...
-          </p>
-
-          <p>
-            Concrètement, à quoi ça nous engage de consommer local ? Nous avons
-            décidé de mettre en place quelques règles de base (pour ne pas
-            tourner en rond pendant des heures dans les rayons des magasins ;)):
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </article>
       </ContentWrapper>

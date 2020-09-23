7ff49aa015148016a457d4edafc75311d8cef105
Jono Hewitt <jonohewitt@gmail.com>
2020-09-23 14:39:40 +0100

    Add sort by FR alphabet if tied by previous sort function

--- src/components/listOfIngredients.js
+++ src/components/listOfIngredients.js
@@ -28,9 +28,15 @@ export const listOfIngredients = ({ filter, sort, monthIndex }) => {
         case "lastChance":
           return monthsUntil("end", ingredient) < 2
         case "justIn":
-          return ingredient.months[monthIndex] && monthsSince("start", ingredient) < 2
+          return (
+            ingredient.months[monthIndex] &&
+            monthsSince("start", ingredient) < 2
+          )
         case "comingUp":
-          return !ingredient.months[monthIndex] && monthsUntil("start", ingredient) < 2
+          return (
+            !ingredient.months[monthIndex] &&
+            monthsUntil("start", ingredient) < 2
+          )
         case "testEmpty":
           return false
         default:
@@ -38,16 +44,27 @@ export const listOfIngredients = ({ filter, sort, monthIndex }) => {
       }
     })
     .sort((a, b) => {
+      let sortValue
       switch (sort) {
         case "newest":
-          return monthsSince("start", a) - monthsSince("start", b)
+          sortValue = monthsSince("start", a) - monthsSince("start", b)
+          break
         case "endingSoonest":
-          return monthsUntil("end", a) - monthsUntil("end", b)
+          sortValue = monthsUntil("end", a) - monthsUntil("end", b)
+          break
         case "startingSoonest":
-          return monthsUntil("start", a) - monthsUntil("start", b)
+          sortValue = monthsUntil("start", a) - monthsUntil("start", b)
+          break
         default:
           //alphabetical in french, catches special characters e.g œ
-          return new Intl.Collator("fr").compare(a, b)
+          sortValue = new Intl.Collator("fr").compare(a.name, b.name)
+          break
+      }
+      // sort by french alphabetical if previous sort function returns a tie
+      if (sortValue !== 0) {
+        return sortValue
+      } else {
+        return new Intl.Collator("fr").compare(a.name, b.name)
       }
     })
 }

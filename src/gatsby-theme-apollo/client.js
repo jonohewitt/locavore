8a6bda6773bb804458b5182fb7836bd5a3e634ec
Jono Hewitt <jonohewitt@gmail.com>
2020-11-23 21:14:45 +0000

    Add explanation of file and folder name and content

--- src/gatsby-theme-apollo/client.js
+++ src/gatsby-theme-apollo/client.js
@@ -1,3 +1,6 @@
+// This is a "shadow file" replacing the client.js included with gatsby-theme-apollo
+// The client export is imported behind the scenes as an argument to an Apollo Provider on the root gatsby element
+
 import fetch from "isomorphic-unfetch"
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 

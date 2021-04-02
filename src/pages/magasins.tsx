25ec2e095f73946dbc8f1e99fb4779d0faf17d71
Jono Hewitt <jonohewitt@gmail.com>
2021-04-02 19:57:06 +0200

    Create placeholder shop page to avoid 404

---
+++ src/pages/magasins.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { ContentWrapper } from "../components/contentWrapper"
+import { SEO } from "../components/seo"
+
+const Magasins = () => {
+  return (
+    <>
+      <SEO title="404: Not found" />
+      <ContentWrapper>
+      <header>
+          <h1>Magasins</h1>
+          <hr />
+        </header>
+        <p>This page hasn't been developed yet - hopefully it's coming soon!</p>
+      </ContentWrapper>
+    </>
+  )
+}
+
+export default Magasins

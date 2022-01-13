00fb3cb048ab1decf2aaf74f39324457da601678
Jono Hewitt <jonohewitt@gmail.com>
2022-01-13 19:24:23 +0100

    Add and update types

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+import { ImageDataLike } from "gatsby-plugin-image"
+
 export type MonthIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
 
 export type ThemeMode = "light" | "dark"
@@ -12,21 +14,52 @@ export interface Frontmatter {
   course: string
   ingredients: string[]
   description?: string
-  feature?: any
+  feature?: ImageDataLike
   featureDescription?: string
   feeds?: number
   linkedRecipes?: string[]
   customSlug?: string
 }
 
-export interface Ingredient {
-  name: string
-  type: string
-  season?: { start: number; end: number }
-  source?: { name: string; link: string }
+export interface BlogFrontmatter {
+  title: string
+  category: string
+  slug: string
+  date: string
+  header?: ImageDataLike
+  headerDescription?: string
+  feature?: ImageDataLike
+  featureDescription?: string
 }
 
 export interface Recipe {
   id: string
   frontmatter: Frontmatter
+}
+
+export interface BlogPost {
+  id: string
+  frontmatter: BlogFrontmatter
+}
+
+export interface Ingredient {
+  name: string
+  type: string
+  season?: { start: MonthIndex; end: MonthIndex }
+  source?: { name: string; link: string }
+}
+
+export interface SeasonalIngredient extends Ingredient {
+  season: { start: MonthIndex; end: MonthIndex }
+}
+
+export interface UserComment {
+  id: number
+  user_id: string
+  comment_text: string
+  slug: string
+  date_added: string
+  public_user_info: {
+    username: string
+  }
 }
